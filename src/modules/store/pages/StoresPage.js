1c1b736866827b8b90310b6a769f9d6fa0961d5e
Dmitriy Tkalich <dmitriy.tkalich.work@gmail.com>
2019-03-12 03:51:53 +0200

    refactor(storepage): changed string to number

--- src/modules/store/pages/StoresPage.js
+++ src/modules/store/pages/StoresPage.js
@@ -16,13 +16,13 @@ type StoresPageProps = {
     name: string,
     newStore: boolean,
     offer: string,
-    deals: string,
+    deals: number,
     logo: string,
     url: string,
   }[],
   categories: {
     title: string,
-    number: string,
+    number: number,
   }[],
 };
 
@@ -52,15 +52,19 @@ StoresPage.Wrapper = styled.section`
   ${breakpoint('xs')`
     padding: 21px 0 75px 0;
   `}
-  @media (min-width: 413px) {
+
+  ${breakpoint('sx')`
     padding: 25px 0 75px 0;
-  }
+  `}
+
   ${breakpoint('md')`
     padding: 36px 0 75px 0;
   `}
+
   ${breakpoint('lg')`
     padding: 41px 0 75px 0;
   `}
+
   ${breakpoint('xl')`
     padding: 57px 0 75px 0;
   `}
@@ -70,9 +74,11 @@ StoresPage.Container = styled.div`
   max-width: 1141px;
   margin: 0 auto;
   padding: 0 15px;
+
   ${breakpoint('sm')`
     padding: 0 40px;
   `}
+
   ${breakpoint('md')`
     padding: 0 15px;
   `}
@@ -85,14 +91,17 @@ StoresPage.Title = styled.h3`
   font-weight: bold;
   font-size: 25px;
   color: #374b5a;
+
   ${breakpoint('xs')`
     display: block;
     font-size: 17px;
     padding: 0;
   `}
-  @media (min-width: 413px) {
+
+  ${breakpoint('sx')`
     font-size: 22px;
-  }
+  `}
+
   ${breakpoint('sm')`
     display: none;
     font-size: 25px;
@@ -103,9 +112,11 @@ StoresPage.Title = styled.h3`
 StoresPage.Box = styled.div`
   display: flex;
   width: 100%;
+
   ${breakpoint('xs')`
     flex-direction: column;
   `}
+
   ${breakpoint('sm')`
     flex-direction: row;
   `}
@@ -117,7 +128,7 @@ StoresPage.defaultProps = {
       name: 'Target',
       logo: targetIcon,
       newStore: true,
-      deals: '258456 Deals',
+      deals: 258456,
       offer: '+12% Cash Back',
       url: 'https://target.com',
     },
@@ -125,7 +136,7 @@ StoresPage.defaultProps = {
       name: 'Welmart',
       logo: walmartIcon,
       newStore: true,
-      deals: '258422 Deals',
+      deals: 258422,
       offer: '+1% Cash Back',
       url: 'https://welmart.com',
     },
@@ -133,17 +144,17 @@ StoresPage.defaultProps = {
       name: 'Macus',
       logo: macysIcon,
       newStore: false,
-      deals: '211156 Deals',
+      deals: 211156,
       offer: '+50% Cash Back',
       url: 'https://macus.com',
     },
   ],
   categories: [
-    { title: 'Accessories', number: '101' },
-    { title: 'Beauty', number: '200002' },
-    { title: 'Clothing', number: '9991' },
-    { title: 'Computers', number: '2' },
-    { title: 'Department', number: '10' },
+    { title: 'Accessories', number: 101 },
+    { title: 'Beauty', number: 200002 },
+    { title: 'Clothing', number: 9991 },
+    { title: 'Computers', number: 2 },
+    { title: 'Department', number: 10 },
   ],
 };
 
