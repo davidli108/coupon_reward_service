fff3cdc0a1dd00b5c195956e8609504bfb9310e5
Dmitriy Tkalich <dmitriy.tkalich.work@gmail.com>
2019-03-15 05:56:47 +0200

    feat(store): add Store model

---
+++ src/modules/store/models/index.js
@@ -0,0 +1,16 @@
+// @flow
+
+export type Store = {
+  name: string,
+  newStore: boolean,
+  offer: string,
+  deals: number,
+  logo: string,
+  url: string,
+  couponActive: boolean
+};
+
+export type Categories = {
+  title: string,
+  number: number,
+};
