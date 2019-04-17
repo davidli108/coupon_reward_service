59a2906c37074246a36b3059a8fc5b8ca5d5897b
Dmitriy Tkalich <dmitriy.tkalich.work@gmail.com>
2019-04-17 10:26:03 +0300

    feat(config): default api url to Piggy production api

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -3,7 +3,7 @@ import * as R from 'ramda';
 
 export default {
   apiUrl: R.defaultTo(
-    'http://33499c7a.ngrok.io',
+    'https://www.joinpiggy.com',
     process.env.REACT_APP_API_URL,
   ),
 };
