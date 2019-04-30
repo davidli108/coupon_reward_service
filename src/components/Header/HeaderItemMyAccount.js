137082461bde32abeae98f47d7b79375bebff35c
Dmitriy Tkalich <dmitriy.tkalich.work@gmail.com>
2019-04-30 11:10:42 +0300

    feat(auth): add header dropdown for authenticated user

---
+++ src/components/Header/HeaderItemMyAccount.js
@@ -0,0 +1,130 @@
+//@flow
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { IoMdArrowDropdown } from 'react-icons/io';
+
+type HeaderItemMyAccountProps = {
+  history: Object,
+  bgColor?: string,
+  hoverBgColor?: string,
+  title: string,
+  logout: Function,
+};
+
+const HeaderItemMyAccount = ({
+  history,
+  bgColor,
+  hoverBgColor,
+  title,
+  logout,
+}: HeaderItemMyAccountProps) => {
+  const [isDrop, setIsDrop] = useState(false);
+
+  /*
+  const items = [
+    {
+      title: 'Earnings',
+      onClick: () =>
+        window.location('https://www.joinpiggy.com/account/earnings'),
+    },
+    {
+      title: 'Invite Friends',
+      onClick: () => window.open('https://www.joinpiggy.com/account/referrals'),
+    },
+    {
+      title: 'Settings',
+      onClick: () =>
+        window.open('https://www.joinpiggy.com/account/preferences'),
+    },
+    {
+      title: 'Sign out',
+      onClick: () => logout(),
+    },
+  ];
+  */
+
+  return (
+    <HeaderItemMyAccount.Wrapper
+      bgColor={bgColor}
+      hoverBgColor={hoverBgColor}
+      onMouseLeave={() => setIsDrop(false)}
+    >
+      <p onMouseOver={() => setIsDrop(true)}>
+        {title}
+        <IoMdArrowDropdown />
+      </p>
+      <HeaderItemMyAccount.DropdownWrapper isShow={isDrop}>
+        {/*items.map(i => (
+          <HeaderItemMyAccount.DropdownItem
+            key={`key_${i.title}`}
+            onClick={() => {
+              i.onClick();
+              setIsDrop(false);
+            }}
+          >
+            {i.title}
+          </HeaderItemMyAccount.DropdownItem>
+        ))*/}
+        <HeaderItemMyAccount.DropdownItem
+          onClick={() => {
+            logout();
+            setIsDrop(false);
+          }}
+        >
+          Sign out
+        </HeaderItemMyAccount.DropdownItem>
+      </HeaderItemMyAccount.DropdownWrapper>
+    </HeaderItemMyAccount.Wrapper>
+  );
+};
+
+HeaderItemMyAccount.Wrapper = styled.div`
+  height: 100%;
+
+  font-weight: bold;
+  background: ${({ bgColor }) => bgColor || '#40c8e5'};
+
+  p {
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+
+    width: 100%;
+    height: 100%;
+    padding: 0 15px;
+
+    color: white;
+    font-size: 20px;
+    font-family: Roboto, sans-serif;
+
+    cursor: pointer;
+
+    &:hover {
+      background: ${({ hoverBgColor }) => hoverBgColor || 'inherit'};
+    }
+  }
+`;
+
+HeaderItemMyAccount.DropdownWrapper = styled.div`
+  display: ${props => (props.isShow ? 'flex' : 'none')};
+  flex-direction: column;
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
+  border-radius: 4px;
+  background-color: white;
+`;
+
+HeaderItemMyAccount.DropdownItem = styled.div`
+  padding: 15px 0 15px 10px;
+  font-size: 20px;
+  font-weight: 500;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #29899e;
+    color: white;
+  }
+`;
+
+export default withRouter(HeaderItemMyAccount);
