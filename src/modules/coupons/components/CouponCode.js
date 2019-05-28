fb2626a67fa5264c14e1a4423dedca8ac02dd8f9
Ionut <ionut@upstack.co>
2019-05-28 13:06:54 +0300

    feat(coupons): installOverlay design and functionality based on userAgent

--- src/modules/coupons/components/CouponCode.js
+++ src/modules/coupons/components/CouponCode.js
@@ -2,34 +2,139 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { withTranslation } from 'react-i18next';
+import ModalActivateCoupons from '../components/ModalActivateCoupons';
+import InstallOverlay from '../components/InstallOverlay';
+import axios from 'axios';
 
 type CouponCodeProps = {
   t: Function,
   code: string,
   link: string,
+  store: string,
+  logo: string,
+  getIsAuthenticated: boolean,
 };
 
-const CouponCode = ({ t, code, link }: CouponCodeProps) => {
-  const [isShowCode, setIsShowCode] = useState(false);
+const isChrome =
+  !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
 
-  return (
-    <CouponCode.Wrapper>
-      <CouponCode.Button
-        onClick={() => {
+const CouponCode = ({
+  t,
+  code,
+  link,
+  store,
+  logo,
+  getIsAuthenticated,
+}: CouponCodeProps) => {
+  const [isShowCode, setIsShowCode] = useState(false);
+  const [showActivateModal, setShowActivateModal] = useState(false);
+  const [modalMounted, setModalMounted] = useState(false);
+  const [isInstallActive, setIsInstallActive] = useState(false);
+
+  const toggleActivateModal = () => {
+    if (modalMounted === true) {
+      setShowActivateModal(!showActivateModal);
+      setTimeout(() => {
+        setModalMounted(!modalMounted);
+
+        if (!isInstallActive) {
+          setIsShowCode(true);
+        }
+      }, 300);
+    } else {
+      setModalMounted(!modalMounted);
+      setTimeout(() => {
+        setShowActivateModal(!showActivateModal);
+      });
+    }
+  };
+
+  const getExtension = () => {
+    return axios.get(
+      'chrome-extension://hfapbcheiepjppjbnkphkmegjlipojba/manifest.json',
+    );
+  };
+
+  const handleClick = () => {
+    getExtension().then(
+      response => {
+        if (response.data.version) {
           if (code) {
             setIsShowCode(true);
           } else {
             window.open(link, '_blank');
           }
-        }}
-        isShow={!isShowCode}
-      >
-        {code ? t('coupons.buttons.viewCoupon') : t('coupons.buttons.viewDeal')}
-      </CouponCode.Button>
-      <CouponCode.Code href={link} target="_blank" isShow={isShowCode}>
-        {code}
-      </CouponCode.Code>
-    </CouponCode.Wrapper>
+        }
+      },
+      () => {
+        if (code) {
+          if (getIsAuthenticated) {
+            setIsShowCode(true);
+          } else {
+            if (isChrome) {
+              toggleActivateModal();
+            } else {
+              setIsShowCode(true);
+            }
+          }
+        } else {
+          window.open(link, '_blank');
+        }
+      },
+    );
+  };
+
+  const modalCallback = () => {
+    const left = window.screenX;
+    const top = 0;
+
+    const popup = window.open(
+      'https://chrome.google.com/webstore/detail/piggy-automatic-coupons-c/hfapbcheiepjppjbnkphkmegjlipojba?hl=en',
+      'extensionWindow',
+      `toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,width=1100,height=700,left=${left},top=${top}`,
+    );
+
+    setIsInstallActive(true);
+    setShowActivateModal(false);
+    setModalMounted(false);
+
+    const focusInterval = setInterval(() => {
+      popup.focus();
+
+      if (popup.closed) {
+        clearInterval(focusInterval);
+        setIsInstallActive(false);
+        setIsShowCode(true);
+      }
+    }, 100);
+  };
+
+  return (
+    <>
+      <CouponCode.Wrapper>
+        <CouponCode.Button onClick={handleClick} isShow={!isShowCode}>
+          {code
+            ? t('coupons.buttons.viewCoupon')
+            : t('coupons.buttons.viewDeal')}
+        </CouponCode.Button>
+        <CouponCode.Code href={link} target="_blank" isShow={isShowCode}>
+          {code}
+        </CouponCode.Code>
+      </CouponCode.Wrapper>
+
+      {modalMounted && (
+        <ModalActivateCoupons
+          isActive={showActivateModal}
+          closeModal={toggleActivateModal}
+          callback={modalCallback}
+          title={store}
+          logo={logo}
+          subTitle={'test'}
+        />
+      )}
+
+      {isInstallActive && <InstallOverlay isActive={isInstallActive} />}
+    </>
   );
 };
 
---
+++ src/modules/coupons/components/InstallOverlay.js
@@ -0,0 +1,72 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+import leftRoundArrow from '../assets/leftRoundArrow.svg';
+
+type InstallOverlayProps = {
+  isActive: boolean,
+};
+
+const InstallOverlay = ({ isActive }: InstallOverlayProps) => (
+  <InstallOverlay.Wrapper isActive={isActive}>
+    <InstallOverlay.Container
+      top={navigator.platform === 'Win32' ? '160px' : '100px'}
+    >
+      <img src={leftRoundArrow} alt="Add to Chrome" />
+
+      <InstallOverlay.Step>
+        <h3>Step 1</h3>
+        <p>Click the "Add to Chrome" Button</p>
+      </InstallOverlay.Step>
+
+      <InstallOverlay.Step>
+        <h3>Step 2</h3>
+        <p>Then click "Add extension"</p>
+      </InstallOverlay.Step>
+    </InstallOverlay.Container>
+  </InstallOverlay.Wrapper>
+);
+
+export default InstallOverlay;
+
+InstallOverlay.Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1050;
+  background-color: rgba(0, 0, 0, 0.9);
+  color: #fff;
+`;
+
+InstallOverlay.Container = styled.div`
+  position: absolute;
+  left: 1130px;
+  top: ${({ top }) => top};
+  width: 320px;
+
+  > img {
+    display: block;
+    width: 75px;
+    height: auto;
+    position: relative;
+    margin: 0 0 17px;
+  }
+`;
+
+InstallOverlay.Step = styled.div`
+  padding: 0 0 0 36px;
+  margin: 0 0 30px;
+
+  h3 {
+    margin: 0 0 5px;
+    font-weight: bold;
+    font-size: 28px;
+  }
+
+  p {
+    margin: 0;
+    font-size: 24px;
+  }
+`;
