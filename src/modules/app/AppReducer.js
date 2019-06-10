7752fd5fc5dbb8aed3eedcf262d1fbc4705fb8a6
John Ginsberg <john.ginsberg@tristadeca.com>
2019-06-10 17:00:17 +0100

    Wim/web interstitial modal (#17)
    * feat(coupons): implement graphic design and relaced with env variable hadcorded store image cloud url
    
    * test(snapshots): update snapshots
    
    * feat(coupons): installOverlay design and functionality based on userAgent
    
    * feat(coupons): steps position based on os and browser layout
    
    * feat(coupons): apply coupons modal on deals, show modal based on set cookie
    
    * feat(coupons): added js-cookie package
    
    * refactor(coupons): modals functionality, removed duplicate code
    
    * test(coupons): added tests for activate and install modals
    
    * feat(coupons): added translations support to activation and install modals
    
    * feat(coupons): modal animations, new window top position based on parent position, noindex and nofolllow for activate and install modals
    
    * fix(ci): fixed ci failures
    
    * feat(translation): applied activate and install modals
    
    * feat(deals): add extension modal to today feature deals
    
    * feat(coupons): check if is desktop, remove install extension banner if extension is installed
    
    * feat(coupons): remove add savings if extension is installed
    
    * fix(footer): fixed footer layout design to match staging, cover mobile, tablet and desktop small screens
    
    * fix(login): login user if cf cookie is present
    
    * fix(coupons): fixed featured coupon on tablet, fixed active filter coupon icon, fixed desktop feature coupons flicker
    
    * fix(login): fixed login/logout based on cf, show all coupons if user is logged in
    
    * refactor(search): refactored search functionality
    
    * fix(ci): fixed ci errors
    
    * fix(ci): fixed ci errors
    
    * refactor(login): refactor login functionality based only on cf cookie
    
    * refactor(login): refactor login flow based on cf cookie
    
    * chore(appSaga): revert code since we use auth based on state management
    
    * fix(auth): fixed checking auth on route change
    
    * feature(search): implemented search functionality on store page
    
    * feature(search): sort results by first word occurence
    
    * fix(ci): fix sort function by passing number instead of boolean

--- src/modules/app/AppReducer.js
+++ src/modules/app/AppReducer.js
@@ -30,8 +30,14 @@ export const STATE_KEY = 'app';
 export const getStoresList = R.path<string>([STATE_KEY, 'stores']);
 export const getFilteredList = (state: any) => (keyword: string) => {
   const regex = new RegExp('\\b' + keyword, 'gi');
+  const sort = new RegExp('^\\b' + keyword, 'i');
+
   return state.app.stores
     .filter(store => regex.test(store.store_name))
+    .sort(
+      (a, b) =>
+        Number(sort.test(b.store_name)) - Number(sort.test(a.store_name)),
+    )
     .slice(0, 5);
 };
 
