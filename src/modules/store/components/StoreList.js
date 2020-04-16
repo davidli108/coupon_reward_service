428d1f34023c427fcb7fe2693287f870bb9cc03e
Rex <rcordovez@joinpiggy.com>
2020-04-16 11:07:18 +0800

    NATE-328 Update

--- src/modules/store/components/StoreList.js
+++ src/modules/store/components/StoreList.js
@@ -5,11 +5,14 @@ import { Link, withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 import breakpoint from 'styled-components-breakpoint';
 import { withTranslation } from 'react-i18next';
-import moment from 'moment';
+import moment, { locale } from 'moment';
 
 import placeholder from '@modules/coupons/assets/image-placeholder.png';
 import LoadMoreLoader from './loaders/LoadMoreLoader';
-import { currencyLocaleFormat } from '@modules/localization/i18n';
+import { 
+  currencyLocaleFormat,
+  setDecimalFormat,
+} from '@modules/localization/i18n';
 import { isAmazonStore } from '@config/Utils';
 
 import { type Store } from '../models';
@@ -93,8 +96,8 @@ const StoreList = ({
             }: Object) => {
               const discount = cashBackOk
                 ? payType === 1
-                  ? currencyLocaleFormat(cashbackSave, country)
-                  : `${cashbackSave}%`
+                  ? currencyLocaleFormat(cashbackSave, country || i18n.language)                 
+                  : `${(getLocale() === Locale.de || getLocale() === Locale.fr) ? cashbackSave.replace(/\./g, ',') : cashbackSave}%`
                 : '';
 
               const cashBackMessageText = noCashBack
