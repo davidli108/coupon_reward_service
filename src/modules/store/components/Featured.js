55b12cb7d6bf0ad4420fc52a80050637f9298960
Rex <rcordovez@joinpiggy.com>
2020-05-20 21:46:56 +0800

    NATE-328 Update Fixes

--- src/modules/store/components/Featured.js
+++ src/modules/store/components/Featured.js
@@ -49,7 +49,7 @@ const Featured = ({ t, featured }: FeaturedProps) => {
             const discount = cashbackok
               ? pay_type === 1
                 ? currencyLocaleFormat(cashback_text, country || i18n.language)
-                : setDecimalFormat(`${cashback_text}%`)
+                : setDecimalFormat(`${cashback_text} %`)
               : '';
             const cashBackMessageKey = cashbackok
               ? pay_type === 1
