1b5e67e32e36cd365bc0292ff05afbd220866675
Ionut Anin <ionutanin@gmail.com>
2020-08-05 20:50:57 +0300

    HOTFIX - Landing page timeout clear (#548)
    * fix(landing): hotfix for landing page timeout
    
    * fix(landing): change timer from 5s to 30s
    
    * fix(coupons): fix coupons page if store doesn't exist then should redirect to coupons page
    
    * fix(store-coupons): apply code length fix if too long
    
    * fix(landing): check if timer is set before clear timeout

--- src/config/Utils.js
+++ src/config/Utils.js
@@ -5,7 +5,7 @@ export const isAmazonStore = (storeName: string) =>
   storeName && storeName.match(/^amazon/i);
 
 export const setDirectTrue = (link: string) =>
-  !link.includes('direct=1') ? `${link}&direct=1` : link;
+  link && !link.includes('direct=1') ? `${link}&direct=1` : link;
 
 export const getStoresWithDirectLinkSet = (stores: Object, link: any) =>
   stores.map(store => ({
